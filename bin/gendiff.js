a32586430ebe9e68ff41f045b708b4efc4b1f0ec
tatiana <alks.tatiana@gmail.com>
2023-12-15 21:50:33 +0700

    add gendiff

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
