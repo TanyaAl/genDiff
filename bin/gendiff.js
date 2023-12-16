c3a441944457a1becaa5579a4736921c395ad1d6
tatiana <alks.tatiana@gmail.com>
2023-12-16 21:05:53 +0700

    add arg & opt

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,10 +2,13 @@
 import { program } from 'commander';
 program
   .name('gendiff')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference')
   .version('1.0.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
-  .parse(process.argv);
+  .option('-f, --format <type>', 'output format')
+
+  .parse(process.argv); 
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
