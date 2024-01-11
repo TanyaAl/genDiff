a45c3facb83ba783ab08335e69ac741f221dcff8
tatiana <alks.tatiana@gmail.com>
2024-01-11 21:07:08 +0700

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,15 @@
 import { readFileSync } from 'node:fs';
 import gendiff from '../src/index.js';
 
+const file1 = './__fixtures__/file1.json';
+const file2 = './__fixtures__/file2.json';
+
 test('compare two JSON with differences', () => {
-  const file1 = './__fixtures__/file1.json';
-  const file2 = './__fixtures__/file2.json';
   const rightOutput = readFileSync('./__fixtures__/testJSONdif.txt', 'utf-8');
   expect(gendiff(file1, file2)).toEqual(rightOutput);
 });
 
 test('compare two JSON without differences', () => {
-  const file1 = './__fixtures__/file1.json';
-  const file2 = './__fixtures__/file1.json';
   const rightOutput = readFileSync('./__fixtures__/testJSONsame.txt', 'utf-8');
-  expect(gendiff(file1, file2)).toEqual(rightOutput);
+  expect(gendiff(file1, file1)).toEqual(rightOutput);
 });
