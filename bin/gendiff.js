7b5353e6a92516c1166f3e5ab476cf05d85d11bf
tatiana <alks.tatiana@gmail.com>
2024-01-24 12:35:47 +0700

    fix import

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 import { program } from 'commander';
-import gendiff from '../src/index';
+import gendiff from '../index';
 
 program
   .name('gendiff')
